10ab58d367cb1db7e5b1694637b933d41b97c943
Katherine Manders <364775@student.fontys.nl>
2017-05-18 15:16:51 +0200

    Product context en sql context

---
+++ ASPEyeCTEvents/ASPEyeCTEvents/App_DAL/IProductContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASPEyeCTEvents.Models;
+
+namespace ASPEyeCTEvents.App_DAL
+{
+    public interface IProductContext
+    {
+        List<Product> GetAllProducts();
+        void Insert(Product product, ProductCategorie productCategorie);
+        void Update(Product product, ProductCategorie productCategorie);
+        void Delete(Product product);
+
+    }
+}
---
+++ ASPEyeCTEvents/ASPEyeCTEvents/App_DAL/IVerhuurContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPEyeCTEvents.App_DAL
+{
+    public interface IVerhuurContext
+    {
+
+    }
+}
