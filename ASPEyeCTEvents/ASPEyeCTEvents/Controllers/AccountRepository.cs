c22c765f8f8ae3295e96a0c5efb52a6e4c173143
Tim S <tim.sanders@student.fontys.nl>
2017-05-18 13:04:31 +0200

    Account + Database (verander connectionstring)

---
+++ ASPEyeCTEvents/ASPEyeCTEvents/Controllers/AccountRepository.cs
@@ -0,0 +1,39 @@
+﻿using ASPEyeCTEvents.App_DAL;
+using ASPEyeCTEvents.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPEyeCTEvents.Controllers
+{
+    public class AccountRepository
+    {
+        private IAccountContext context;
+
+        public AccountRepository(IAccountContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Account> GetAllAccounts()
+        {
+            return context.GetAllAccounts();
+        }
+
+        public Account InsertAccount(Account account)
+        {
+            return context.InsertAccount(account);
+        }
+
+        public bool UpdateAccount(Account account)
+        {
+            return context.UpdateAccount(account);
+        }
+
+        public bool DeleteAccount(int id)
+        {
+            return context.DeleteAccount(id);
+        }
+    }
+}
